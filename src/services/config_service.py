ddf31b56276256ebee236b9db4972e4800484f93
rnk0085 <rinawkmbi4k8@gmail.com>
2025-02-17 00:05:23 +0900

    feat: TestConfigService を追加

--- src/services/config_service.py
+++ src/services/config_service.py
@@ -12,13 +12,17 @@ class ConfigService:
         return os.getenv(f'DISCORD_{mbti_type}_TOKEN')
     
     def get_channel_id(self, channel_name: str) -> int:
-        return int(os.getenv(f'{channel_name}_CHANNEL_ID'))
+        try:
+            return int(os.getenv(f'{channel_name}_CHANNEL_ID'))
+        except Exception as e:
+            print(f"Error = {e}: チャンネルIDの取得に失敗しました")
+            return 0
     
     def get_reaction_rate(self) -> float:
-        return float(os.getenv('REACTION_RATE', '0.5')) 
+        return float(os.getenv('REACTION_RATE', 0.5)) 
     
     def get_auto_reply_rate(self) -> float:
-        return float(os.getenv('AUTO_REPLY_RATE', '0.75'))
+        return float(os.getenv('AUTO_REPLY_RATE', 0.75))
     
     def get_auto_reply_in_free_talk_rate(self) -> float:
-        return float(os.getenv('AUTO_REPLY_IN_FREE_TALK_RATE', '0.1'))
+        return float(os.getenv('AUTO_REPLY_IN_FREE_TALK_RATE', 0.1))
